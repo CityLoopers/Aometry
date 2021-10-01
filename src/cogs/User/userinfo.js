a95dfd460da448dd1c453fafbbfcf3450cae15e7
finneh <ethancornwill@outlook.com>
2021-10-01 14:54:09 +1000

    Sorta fixed userinfo

--- src/cogs/User/userinfo.js
+++ src/cogs/User/userinfo.js
@@ -1,5 +1,5 @@
 /*
-This command isn't currently working, so we'll fix it later xxx :)
+This command isn't currently working, so we'll fix it later xxx :)*/
 
 const { Client, MessageEmbed, CommandInteraction } = require('discord.js');
 const moment = require('moment');
@@ -18,17 +18,17 @@ module.exports = {
      * @param {Client} client 
      * @param {CommandInteraction} interaction 
      */
-/*execute(interaction, client) {
+    execute(interaction, client) {
         const Target = interaction.options.getUser('user') || interaction.user;
 
         const Response = new MessageEmbed()
             .setAuthor(`${Target.user}`, Target.displayAvatarURL({ dynamic: true }))
             .setThumbnail(Target.displayAvatarURL({ dynamic: true }))
             .setColor('YELLOW')
-            .addField("UserID", `${Target.botid}`, false)
-            .addField("Roles", `${Target.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`)
+            .addField("UserID", `${Target.id}`, false)
+            //.addField("Roles", `${Target.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`)
             .addField("Member Since", `${moment(Target.joinedAt).format('Do MMMM YYYY, h:mm:ss a')}\n**-** ${moment(Target.joinedAt).startOf('day').fromNow()}`)
             .addField("Discord User Since", `${moment(Target.createdAt).format('Do MMMM YYYY, h:mm:ss a')}\n**-** ${moment(Target.createdAt).startOf('day').fromNow()}`)
         interaction.reply({ embeds: [Response] })
     }
-}*/
+}
