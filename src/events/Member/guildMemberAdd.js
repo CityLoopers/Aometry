d3d4f6b295e6e1053af9ae968e270887ed25c6ee
finneh <ethancornwill@outlook.com>
2021-10-01 20:30:27 +1000

    begining of database connection for config

--- src/events/Member/guildMemberAdd.js
+++ src/events/Member/guildMemberAdd.js
@@ -1,4 +1,5 @@
 const { MessageEmbed, WebhookClient, GuildMember } = require('discord.js');
+const guildConfig = require('../../schemas/guildConfig')
 
 module.exports = {
     name: "guildMemberAdd",
@@ -7,13 +8,17 @@ module.exports = {
      */
     execute(member) {
         const { user, guild } = member
+
+        /* guildConfig.findOne({ 
+             guildId: interaction.guildId
+         })*/
         let welcomeChannel = guild.channels.cache.find((channel) => channel.name.toLowerCase() === `welcome`)
         let logsChannel = guild.channels.cache.find((channel) => channel.name.toLowerCase() === `logs`)
 
-        //const Welcomer = new WebhookClient({
-        //   id: '892739765806456834',
-        //    token: 'urtpc4rUngjxIbrJplYN8Q0zq7hCkoV4G-heW_hfkZamtP2A4KlimimiNNoma8ePLjLg',
-        //});
+        /*const Welcomer = new WebhookClient({
+            id: '892739765806456834',
+            token: 'urtpc4rUngjxIbrJplYN8Q0zq7hCkoV4G-heW_hfkZamtP2A4KlimimiNNoma8ePLjLg',
+        //});*/
         const Welcome = new MessageEmbed()
             .setColor('RANDOM')
             .setAuthor(`🎉 Welcome! 🎉`, user.avatarURL({ dynamic: true, size: 512 }))
