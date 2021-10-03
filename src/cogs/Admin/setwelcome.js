37e38b5da372c09787e6aeda6b70fadc789f451a
finneh4249 <ethancornwill@outlook.com>
2021-10-03 16:03:39 +1100

    changes to setwelcome (still doesn't work)

--- src/cogs/Admin/setwelcome.js
+++ src/cogs/Admin/setwelcome.js
@@ -6,71 +6,44 @@ module.exports = {
     description: "Set your Welcome Channel",
     permission: "ADMINISTRATOR",
     options: [{
-        name: 'channel',
-        description: 'Set a channel',
-        type: 'SUB_COMMAND',
-        options: [{
             name: 'channel',
             description: 'Set a channel',
             type: 'CHANNEL',
             required: true,
-        }]
-    }, {
-        name: 'message',
-        description: 'Set your welcome message',
-        type: 'SUB_COMMAND',
-        options: [{
+        },
+        {
             name: 'message',
             description: 'Set your welcome message',
             type: 'STRING',
             required: true,
-        }]
-    }, ],
+        }
+    ],
+
     /**
      * @param {CommandInteraction} interaction
      * @param {Guild} guild
      */
     async execute(interaction, guild) {
-        const Sub = interaction.options.getSubcommand(['channel', 'message'])
+        const welcMessage = interaction.options.getString('message');
+        const welcChannel = interaction.options.getString('channel');
 
-        if (Sub === 'channel') {
-            const welcChannel = interaction.options.getChannel('channel');
-            guildConfig.findOne({
-                guildId: interaction.guildId,
-            }, async(err, data) => {
-                if (err) console.log(err);
-                if (!data) {
-                    data = new guildConfig({
-                        guildId: interaction.guildId,
-                        guildName: interaction.guildName,
-                        welcomeData: [{
-                            welcomeChannel: welcChannel.id,
-                            welcomeMessage: welcMessage,
-                        }]
-                    });
-                }
-                data.save();
-            });
-            interaction.reply({ embeds: [new MessageEmbed().setDescription(`Channel set to: ${welcChannel}`)] })
-        } else if (Sub === 'message') {
-            const welcMessage = interaction.options.getString('message');
-            guildConfig.findOne({
+        guildConfig.findOne({
+            guildId: interaction.guildId,
+        }, async(err, data) => {
+            if (err) console.log(err);
+            if (!data) {
+                data = new guildConfig({
                     guildId: interaction.guildId,
-                    welcomeMessage: welcMessage,
-                }, async(err, data) => {
-                    if (err) console.log(err);
-                    if (!data) {
-                        data = new guildConfig({
-                            guildId: interaction.guildId,
-                            guildName: interaction.guildName,
-                            welcomeMessage: welcMessage,
-                        })
-                    }
-                    data.save();
-                }
-
-            );
-            interaction.reply({ embeds: [new MessageEmbed().setDescription(`Message set to: ${welcMessage}`)] })
-        }
+                    guildName: interaction.guildName,
+                    welcomeData: [{
+                        welcomeChannel: welcChannel.id,
+                        welcomeMessage: welcMessage,
+                    }]
+                });
+            }
+            data.save();
+        });
+        interaction.reply({ embeds: [new MessageEmbed().setDescription(`Channel set to: ${welcChannel}, Message set to: ${welcMessage}`)] })
     }
+
 }
