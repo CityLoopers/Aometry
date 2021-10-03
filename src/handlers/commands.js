bbdae0e1b6598660aef1a0ea1a1e924037dcbb86
finneh4249 <ethancornwill@outlook.com>
2021-10-03 18:54:10 +1100

    included a new guild into handler.

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -39,15 +39,16 @@ module.exports = async(client) => {
     //PERMISSIONS CHECK//
 
     client.on("ready", async() => {
-        const MainGuild = await client.guilds.cache.get('882220041477709856');
-        const TestGuild = await client.guilds.cache.get('893392409088643124');
+        const guildSupport = await client.guilds.cache.get('882220041477709856');
+        const guildTest = await client.guilds.cache.get('893392409088643124');
+        const guildEnroute = await client.guilds.cache.get('636354429049896991');
 
-        MainGuild.commands.set(CommandsArray).then(async(command) => {
+        guildSupport.commands.set(CommandsArray).then(async(command) => {
             const Roles = (commandName) => {
                 const cmdPerms = CommandsArray.find((c) => c.name === commandName).permission;
                 if (!cmdPerms) return null;
 
-                return MainGuild.roles.cache.filter((r) => r.permissions.has(cmdPerms));
+                return guildSupport.roles.cache.filter((r) => r.permissions.has(cmdPerms));
             }
 
             const fullPermissions = command.reduce((accumulator, r) => {
@@ -61,29 +62,52 @@ module.exports = async(client) => {
                 return [...accumulator, { id: r.id, permissions }]
             }, [])
 
-            await MainGuild.commands.permissions.set({ fullPermissions })
+            await guildSupport.commands.permissions.set({ fullPermissions })
         })
-        TestGuild.commands.set(CommandsArray).then(async(command) => {
-            const Roles = (commandName) => {
-                const cmdPerms = CommandsArray.find((c) => c.name === commandName).permission;
-                if (!cmdPerms) return null;
+        guildTest.commands.set(CommandsArray).then(async(command) => {
+                const Roles = (commandName) => {
+                    const cmdPerms = CommandsArray.find((c) => c.name === commandName).permission;
+                    if (!cmdPerms) return null;
 
-                return TestGuild.roles.cache.filter((r) => r.permissions.has(cmdPerms));
-            }
+                    return guildTest.roles.cache.filter((r) => r.permissions.has(cmdPerms));
+                }
 
-            const fullPermissions = command.reduce((accumulator, r) => {
-                const roles = Roles(r.name);
-                if (!roles) return accumulator;
+                const fullPermissions = command.reduce((accumulator, r) => {
+                    const roles = Roles(r.name);
+                    if (!roles) return accumulator;
 
-                const permissions = roles.reduce((a, r) => {
-                    return [...a, { id: r.id, type: "ROLE", permission: true }]
+                    const permissions = roles.reduce((a, r) => {
+                        return [...a, { id: r.id, type: "ROLE", permission: true }]
+                    }, [])
+
+                    return [...accumulator, { id: r.id, permissions }]
                 }, [])
 
-                return [...accumulator, { id: r.id, permissions }]
-            }, [])
+                await guildTest.commands.permissions.set({ fullPermissions })
+            })
+            /* guildEnroute.commands.set(CommandsArray).then(async(command) => {
+                 const Roles = (commandName) => {
+                     const cmdPerms = CommandsArray.find((c) => c.name === commandName).permission;
+                     if (!cmdPerms) return null;
 
-            await TestGuild.commands.permissions.set({ fullPermissions })
-        })
+                     return guildEnroute.roles.cache.filter((r) => r.permissions.has(cmdPerms));
+                 }
+
+                 const fullPermissions = command.reduce((accumulator, r) => {
+                     const roles = Roles(r.name);
+                     if (!roles) return accumulator;
+
+                     const permissions = roles.reduce((a, r) => {
+                         return [...a, { id: r.id, type: "ROLE", permission: true }]
+                     }, [])
+
+                     return [...accumulator, { id: r.id, permissions }]
+                 }, [])
+
+                 await guildEnroute.commands.permissions.set({ fullPermissions })
+             })*/
     })
+
+
 
 }
