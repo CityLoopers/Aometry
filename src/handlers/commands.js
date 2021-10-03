ecc84155fc3bb0a160724d91620923d7430771ce
finneh4249 <ethancornwill@outlook.com>
2021-10-03 17:02:06 +1100

    Multi-Guild commands

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -40,7 +40,7 @@ module.exports = async(client) => {
 
     client.on("ready", async() => {
         const MainGuild = await client.guilds.cache.get('882220041477709856');
-
+        const TestGuild = await client.guilds.cache.get('893392409088643124');
 
         MainGuild.commands.set(CommandsArray).then(async(command) => {
             const Roles = (commandName) => {
@@ -62,6 +62,27 @@ module.exports = async(client) => {
             }, [])
 
             await MainGuild.commands.permissions.set({ fullPermissions })
+        })
+        TestGuild.commands.set(CommandsArray).then(async(command) => {
+            const Roles = (commandName) => {
+                const cmdPerms = CommandsArray.find((c) => c.name === commandName).permission;
+                if (!cmdPerms) return null;
+
+                return TestGuild.roles.cache.filter((r) => r.permissions.has(cmdPerms));
+            }
+
+            const fullPermissions = command.reduce((accumulator, r) => {
+                const roles = Roles(r.name);
+                if (!roles) return accumulator;
+
+                const permissions = roles.reduce((a, r) => {
+                    return [...a, { id: r.id, type: "ROLE", permission: true }]
+                }, [])
+
+                return [...accumulator, { id: r.id, permissions }]
+            }, [])
+
+            await TestGuild.commands.permissions.set({ fullPermissions })
         })
     })
 
