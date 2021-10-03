bd608e275179e1b8b415cee342a1896bf720f535
finneh4249 <ethancornwill@outlook.com>
2021-10-04 03:40:11 +1100

    literally all the code in this branch is useless

--- src/legacycogs/emitters/memberAdd.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'emitadd',
-    permissions: 'ADMINISTRATOR',
-    description: "Emits the guildMemberAdd Event",
-    execute(message, args, commandName, client, Discord) {
-        client.emit("guildMemberAdd", message.member);
-
-    }
-}
--- src/legacycogs/emitters/memberRemove.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'emitremove',
-    permissions: 'ADMINISTRATOR',
-    description: "Emits the guildMemberRemove Event",
-    execute(message, args, commandName, client, Discord) {
-        client.emit("guildMemberRemove", message.member);
-    }
-}
