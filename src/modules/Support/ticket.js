61b83c85e776539c7517472095fec9738ea3e3f5
finneh4249 <ethancornwill@outlook.com>
2021-10-04 16:37:57 +1100

    Style Changes using eslint

--- src/modules/Support/ticket.js
+++ src/modules/Support/ticket.js
@@ -1,99 +1,99 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 const {
-    Client,
-    CommandInteraction,
-    MessageEmbed,
-    MessageActionRow,
-    MessageButton,
-    Message,
-    ButtonInteraction,
-    ThreadChannel
+  Client,
+  CommandInteraction,
+  MessageEmbed,
+  MessageActionRow,
+  MessageButton,
+  Message,
+  ButtonInteraction,
+  ThreadChannel
 } = require('discord.js')
 
 module.exports = {
-    name: 'ticket',
-    description: 'Opens a ticket',
-    options: [{
-        name: 'reason',
-        description: 'Provide a reason for your ticket',
-        type: 'STRING',
-        required: true,
-        choices: [{
-                name: 'General Inquiry',
-                value: 'General Inquiry'
-            },
-            {
-                name: 'Bug Report',
-                value: 'Bug Report'
-            },
-            {
-                name: 'Feature Request',
-                value: 'Feature Request'
-            }
-        ]
-    }],
+  name: 'ticket',
+  description: 'Opens a ticket',
+  options: [{
+    name: 'reason',
+    description: 'Provide a reason for your ticket',
+    type: 'STRING',
+    required: true,
+    choices: [{
+      name: 'General Inquiry',
+      value: 'General Inquiry'
+    },
+    {
+      name: 'Bug Report',
+      value: 'Bug Report'
+    },
+    {
+      name: 'Feature Request',
+      value: 'Feature Request'
+    }
+    ]
+  }],
 
-    /**
+  /**
      *
      * @param {Client} client
      * @param {CommandInteraction} interaction
      * @param {Guild} guild
      * @param {Message} message
      */
-    async execute(interaction, client, message) {
-        const result = Math.random()
-            .toString(36)
-            .substring(2, 7)
-        const User = interaction.user;
-        const logsChannelId = '889175556254498886'
-        const logsChannel = interaction.guild.channels.cache.get(logsChannelId)
-        const supportChannelId = '887549849002733598'
-        const supportChannel = interaction.guild.channels.cache.get(
-            supportChannelId
-        )
-        const ticketChannel = await supportChannel.threads.create({
-            name: `${User.tag}-${result}`,
-            autoArchiveDuration: 60,
-            reason: `User created a ticket`
-        })
-        const ticketReason = interaction.options.getString('reason')
+  async execute (interaction, client, message) {
+    const result = Math.random()
+      .toString(36)
+      .substring(2, 7)
+    const User = interaction.user
+    const logsChannelId = '889175556254498886'
+    const logsChannel = interaction.guild.channels.cache.get(logsChannelId)
+    const supportChannelId = '887549849002733598'
+    const supportChannel = interaction.guild.channels.cache.get(
+      supportChannelId
+    )
+    const ticketChannel = await supportChannel.threads.create({
+      name: `${User.tag}-${result}`,
+      autoArchiveDuration: 60,
+      reason: 'User created a ticket'
+    })
+    const ticketReason = interaction.options.getString('reason')
 
-        const newTicket = new MessageEmbed()
-            .setColor('YELLOW')
-            .setAuthor(
-                'New Ticket Created',
+    const newTicket = new MessageEmbed()
+      .setColor('YELLOW')
+      .setAuthor(
+        'New Ticket Created',
                 `${User.displayAvatarURL({ dynamic: true })}`
-            )
-            .setDescription(`Ticket Created by ${User}`)
-            .addField('Reason', `${ticketReason}`)
-            .addField('Ticket Channel', `${ticketChannel}`)
-            .setThumbnail(`${User.displayAvatarURL({ dynamic: true })}`)
-        logsChannel.send({ embeds: [newTicket] })
-        interaction.reply(`A ticket has been created! Check it out ${ticketChannel}`)
-        const ticketManage = new MessageEmbed()
-            .setColor('YELLOW')
-            .setAuthor('Thanks for creating a ticket!')
-            .setThumbnail(`${User.displayAvatarURL({ dynamic: true })}`)
-            .setDescription(`Ticket Created by ${User}`)
-            .addField('Reason', `${ticketReason}`)
-            .addField('While you\'re waiting...','Leave a description of your issue and we\'ll get to it ASAP')
-            .addField('Close this ticket', `Close this ticket by clicking the 🔒`)
-            
+      )
+      .setDescription(`Ticket Created by ${User}`)
+      .addField('Reason', `${ticketReason}`)
+      .addField('Ticket Channel', `${ticketChannel}`)
+      .setThumbnail(`${User.displayAvatarURL({ dynamic: true })}`)
+    logsChannel.send({ embeds: [newTicket] })
+    interaction.reply(`A ticket has been created! Check it out ${ticketChannel}`)
+    const ticketManage = new MessageEmbed()
+      .setColor('YELLOW')
+      .setAuthor('Thanks for creating a ticket!')
+      .setThumbnail(`${User.displayAvatarURL({ dynamic: true })}`)
+      .setDescription(`Ticket Created by ${User}`)
+      .addField('Reason', `${ticketReason}`)
+      .addField('While you\'re waiting...', 'Leave a description of your issue and we\'ll get to it ASAP')
+      .addField('Close this ticket', 'Close this ticket by clicking the 🔒')
 
-        const CloseTicket = new MessageActionRow().addComponents(
-            new MessageButton()
-            .setCustomId('closeticket')
-            .setLabel(`🔒 Close Ticket`)
-            .setStyle('SUCCESS')
-        )
-        ticketChannel.send({ content: `<@&882220499927699456> ${User}`, embeds: [ticketManage] }).then(sentMessage => {
-            sentMessage.react(`🔒`)
-        });
-        try {
-            let collected = await message.awaitReactions({filter, max: 1, time: 60000, errors: ['time'] })
-            let reaction = collected.first()
-          } catch (e) {
-            // Did not react in time
-          }
-          
+    const CloseTicket = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId('closeticket')
+        .setLabel('🔒 Close Ticket')
+        .setStyle('SUCCESS')
+    )
+    ticketChannel.send({ content: `<@&882220499927699456> ${User}`, embeds: [ticketManage] }).then(sentMessage => {
+      sentMessage.react('🔒')
+    })
+    try {
+      const collected = await message.awaitReactions({ filter, max: 1, time: 60000, errors: ['time'] })
+      const reaction = collected.first()
+    } catch (e) {
+      // Did not react in time
     }
+  }
 }
--- src/modules/Utilities/invite.js
+++ src/modules/Utilities/invite.js
@@ -1,34 +1,34 @@
-const { MessageEmbed, MessageActionRow, MessageButton, CommandInteraction } = require("discord.js");
+/* eslint-disable no-unused-vars */
+const { MessageEmbed, MessageActionRow, MessageButton, CommandInteraction } = require('discord.js')
 module.exports = {
-    name: "invite",
-    description: "Invite me to your server!",
+  name: 'invite',
+  description: 'Invite me to your server!',
 
-    /**
-     * 
-     * @param {Client} client 
+  /**
+     *
+     * @param {Client} client
      * @param {CommandInteraction} interaction
      */
 
-    async execute(interaction, client) {
+  async execute (interaction, client) {
+    const Invite = new MessageEmbed()
+      .setTitle('Invite Me!')
+      .setDescription("I'm a cool Discord Bot, ain't I? Use the buttons below to invite me to your server or join our support server!\n\nStay Safe 👋")
+      .setColor('YELLOW')
+      .setThumbnail(client.user.displayAvatarURL())
 
-        const Invite = new MessageEmbed()
-            .setTitle("Invite Me!")
-            .setDescription("I'm a cool Discord Bot, ain't I? Use the buttons below to invite me to your server or join our support server!\n\nStay Safe 👋")
-            .setColor("YELLOW")
-            .setThumbnail(client.user.displayAvatarURL())
+    const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setURL('https://discord.com/api/oauth2/authorize?client_id=887632365726351371&permissions=8&scope=bot%20applications.commands')
+        .setLabel('Invite Me')
+        .setStyle('LINK'),
 
-        let row = new MessageActionRow().addComponents(
-            new MessageButton()
-            .setURL("https://discord.com/api/oauth2/authorize?client_id=887632365726351371&permissions=8&scope=bot%20applications.commands")
-            .setLabel("Invite Me")
-            .setStyle("LINK"),
+      new MessageButton()
+        .setURL('https://discord.gg/zturVQrhTG')
+        .setLabel('Support Server')
+        .setStyle('LINK')
 
-            new MessageButton()
-            .setURL("https://discord.gg/zturVQrhTG")
-            .setLabel("Support Server")
-            .setStyle("LINK"),
-
-        );
-        interaction.reply({ embeds: [Invite], components: [row] });
-    }
-};
+    )
+    interaction.reply({ embeds: [Invite], components: [row] })
+  }
+}
