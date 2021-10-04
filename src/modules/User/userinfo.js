914875c4323be62d64d048e7a3a409a2f8f8bf8d
finneh4249 <ethancornwill@outlook.com>
2021-10-04 22:53:34 +1100

    disabled some commands pending removal

--- src/modules/User/userinfo.js
+++ src/modules/User/userinfo.js
@@ -26,7 +26,7 @@ module.exports = {
       .setAuthor(`${Target.user}`, Target.displayAvatarURL({ dynamic: true }))
       .setThumbnail(Target.displayAvatarURL({ dynamic: true }))
       .setColor('YELLOW')
-      .addField('UserID', `${Target.id}`, false)
+      .addField('UserID', `${Target.id}`)
     // .addField("Roles", `${Target.roles.cache.map(r => r).join(' ').replace("@everyone", " ")}`)
       .addField('Member Since', `${moment(Target.joinedAt).format('Do MMMM YYYY, h:mm:ss a')}\n**-** ${moment(Target.joinedAt).startOf('day').fromNow()}`)
       .addField('Discord User Since', `${moment(Target.createdAt).format('Do MMMM YYYY, h:mm:ss a')}\n**-** ${moment(Target.createdAt).startOf('day').fromNow()}`)
