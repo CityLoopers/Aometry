0e23e85e1c395a079ced5d56f759a43889de8cf2
finneh4249 <ethancornwill@outlook.com>
2021-10-04 15:52:20 +1100

    Display info on servers that aometry is connected

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -10,8 +10,23 @@ module.exports = {
      * @param {Client} client 
      */
     execute(client) {
-        console.log(`✅ Aometry v21.10.0 Ready!`);
-        client.user.setActivity("to /help", { type: 'LISTENING' });
+        console.log(`✅ ${client.user.username} is now online.`)
+        client.guilds.cache.forEach((guild) =>{
+            console.log(`Connected to: ${guild.name} | ID: ${guild.id}`)});
+    
+    const activites = [
+        {name: `/help | ${client.guilds.cache.size} servers!`, type: "WATCHING"},
+        {name: `/help | ${client.users.cache.size} users!`, type: "LISTENING"},
+        {name: `/links | ${client.ws.ping}ms`, type: "LISTENING"},
+        {name: `/botinfo | commands!`, type: "LISTENING"},
+    ]
+    let activity = 0
+    client.user.setPresence({status: "online", activity: activites[0]})
+    setInterval(() => {
+        if(activity === activity.length) return activity = 0;
+        activity++
+        client.user.setActivity(activites[Math.floor(Math.random() * activites.length)])
+    }, 1000 * 35);
 
         if (!DBURL) return;
         mongoose.connect(DBURL, {
---
+++ src/modules/Utilities/myservers.js
@@ -0,0 +1,16 @@
+const { CommandInteraction, MessageEmbed, Client } = require('discord.js')
+
+module.exports = {
+    /**
+    * @param {CommandInteraction} interaction
+    * @param {Client} client
+    */
+   name: 'myservers',
+   description: 'Displays all servers that Aometry is connected to',
+
+   async execute(interaction, client){
+        client.guilds.cache.forEach((guild) =>{
+            interaction.channel.send(`\`${guild.name}\` | ID: \`${guild.id}\``)
+        }); interaction.reply('Here you go!');
+   }
+}
