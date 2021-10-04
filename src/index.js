273de438c08d594253f69a0d0631c0789142d53f
finneh4249 <ethancornwill@outlook.com>
2021-10-04 22:51:20 +1100

    error handling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { Client, Collection } = require('discord.js')
+const Errorhandler = require('discord-error-handler')
 const client = new Client({
   intents: 32767,
   partials: ['CHANNEL']
@@ -8,5 +9,12 @@ const { token } = require('./config.json')
 client.commands = new Collection()
 require('./handlers/events')(client)
 require('./handlers/commands')(client)
+
+const handle = new Errorhandler(client, {
+  webhook: { id: '894549337894424616', token: 'KoOZsBuLtzWUdlKh3A3jFNXv0yxxTFhFikEImHkjCAxRLSZsQsuJ_fNtvdkEDB_N7qlr' }
+})
+process.on('unhandledRejection', error => {
+  handle.createrr(client, undefined, undefined, error)
+})
 
 client.login(token)
