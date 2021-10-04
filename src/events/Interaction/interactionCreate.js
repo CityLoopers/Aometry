7c60a721c785bfe9e96364220957c3f63be6bc4a
finneh4249 <ethancornwill@outlook.com>
2021-10-04 22:28:00 +1100

    implemented button handler

--- src/events/Interaction/interactionCreate.js
+++ src/events/Interaction/interactionCreate.js
@@ -1,5 +1,11 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable new-cap */
+/* eslint-disable no-case-declarations */
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-const { Client, CommandInteraction, MessageEmbed } = require('discord.js')
+const { Client, CommandInteraction, MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
+const generateTranscript = require('reconlx')
+const db = require('quick.db')
 
 module.exports = {
   /**
@@ -7,7 +13,10 @@ module.exports = {
      * @param {Client} client
      */
   name: 'interactionCreate',
-  async execute (interaction, client) {
+  async execute (interaction, client, message) {
+    const guildConfig = new db.table(`guildConfig_${interaction.guild.id}`)
+    const ownerDM = await interaction.guild.fetchOwner()
+    // Slash Command Handler
     if (interaction.isCommand()) {
       const command = client.commands.get(interaction.commandName)
       if (!command) {
@@ -22,6 +31,49 @@ module.exports = {
       }
 
       command.execute(interaction, client)
+    }
+    // Button Handler
+    if (interaction.isButton()) {
+      switch (interaction.customId) {
+        case 'closeTicket':
+          const embed = new MessageEmbed()
+            .setTitle('Are you sure?')
+            .setDescription('Closing a ticket is permanent, are you sure your query is resolved?')
+
+          const row = new MessageActionRow().addComponents(
+            new MessageButton()
+              .setCustomId('imSure')
+              .setLabel('I\'m Sure!')
+              .setStyle('SUCCESS')
+          )
+          interaction.channel.send({ embeds: [embed], components: [row] })
+          break
+
+        case 'imSure':
+          const logsChannelId = guildConfig.get('logsChannel') || ownerDM.send(`Tried to send a logs message but no channel was defined. Please use \`/guild-config\` in the guild: ${guild}`)
+          const ticketClosed = new MessageEmbed()
+            .setColor('GREEN')
+            .setAuthor(
+              'Ticket Closed',
+                `${interaction.user.displayAvatarURL({ dynamic: true })}`
+            )
+            .setDescription(`Ticket Created by ${interaction.user}`)
+            .addField('Ticket Channel', `${interaction.channel}`)
+            .setThumbnail(`${interaction.user.displayAvatarURL({ dynamic: true })}`)
+
+          if (typeof logsChannelId === 'string') {
+            const logsChannel = interaction.guild.channels.cache.get(logsChannelId)
+            logsChannel.send({ embeds: [ticketClosed] })
+          } else {
+            logsChannelId
+          }
+          interaction.channel.setArchived()
+          break
+
+        default:
+          console.log(interaction)
+          break
+      }
     }
   }
 }
