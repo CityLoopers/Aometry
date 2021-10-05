9c2885d76496280503c42cdb91b47ce52a9cb685
finneh4249 <ethancornwill@outlook.com>
2021-10-05 16:02:02 +1100

    anime blush command

---
+++ src/modules/Fun/blush.js
@@ -0,0 +1,18 @@
+const anime = require('anime-actions')
+const { CommandInteraction, MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: 'blush',
+  description: 'get blush gif',
+  /**
+  * @param {CommandInteraction} interaction
+  * @param {Client} client
+  */
+  async execute (interaction) {
+    const embed = new MessageEmbed()
+      .setTitle(`${interaction.user.tag} blushes...`)
+      .setImage(await anime.blush())
+      .setColor('#03fcf8')
+    interaction.reply({ embeds: [embed] })
+  }
+}
