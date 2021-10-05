1c81fc16e801cb3a1eeea1479225c2648b6af9ce
finneh4249 <ethancornwill@outlook.com>
2021-10-05 16:02:32 +1100

    guild emoji display command

---
+++ src/modules/Fun/emojis.js
@@ -0,0 +1,37 @@
+const { CommandInteraction, MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: 'emojis',
+  description: 'View all emojis in the guild',
+  /**
+  * @param {CommandInteraction} interaction
+  * @param {Client} client
+  */
+  async execute (interaction, client) {
+    let Emojis = ''
+    let EmojisAnimated = ''
+    let EmojiCount = 0
+    let Animated = 0
+    let OverallEmojis = 0
+    function Emoji (id) {
+      return client.emojis.cache.get(id).toString()
+    }
+    interaction.guild.emojis.cache.forEach((emoji) => {
+      OverallEmojis++
+      if (emoji.animated) {
+        Animated++
+        EmojisAnimated += Emoji(emoji.id)
+      } else {
+        EmojiCount++
+        Emojis += Emoji(emoji.id)
+      }
+    })
+    const Embed1 = new MessageEmbed()
+      .setTitle(`Emojis in ${interaction.guild.name}.`)
+      .setDescription(
+        `**Animated \`${Animated}\`**:\n${EmojisAnimated}\n\n\n**Standard \`${EmojiCount}\`**:\n${Emojis}\n\n\n**Overall emojis \`${OverallEmojis}\`**`
+      )
+      .setColor('RANDOM')
+    interaction.reply({ embeds: [Embed1] })
+  }
+}
