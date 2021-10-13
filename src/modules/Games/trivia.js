0eee9038a3d565297aeea428b141251dadfefed1
finneh4249 <ethancornwill@outlook.com>
2021-10-13 13:31:07 +1100

    New Games

---
+++ src/modules/Games/trivia.js
@@ -0,0 +1,21 @@
+const { Trivia } = require('discord-gamecord')
+
+module.exports = {
+  name: 'trivia',
+  description: 'Trivia',
+  async execute (interaction, client) {
+    new Trivia({
+      message: interaction,
+      slash_command: true,
+      embed: {
+        title: 'Trivia',
+        description: 'You have {time} seconds to respond!',
+        color: '#5865F2'
+      },
+      difficulty: 'medium',
+      winMessage: 'GG, Your answer was correct! It was **{answer}**',
+      loseMessage: 'Your answer was Incorrect! The correct answer was **{answer}**',
+      othersMessage: 'You are not allowed to use buttons for this message!'
+    }).startGame()
+  }
+}
