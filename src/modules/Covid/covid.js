40b4615ff4ca1f892ac24e6278fcd5a1daa805b6
finneh4249 <ethancornwill@outlook.com>
2021-10-17 23:51:36 +1100

    more compact (and more readable) code!

--- src/modules/Covid/covid.js
+++ src/modules/Covid/covid.js
@@ -13,34 +13,17 @@ module.exports = {
     description: 'Display covid information about the specified state',
     type: 'STRING',
     required: true,
-    choices: [{
-      name: 'Australia',
-      value: '0'
-    }, {
-      name: 'Australian Capital Territory',
-      value: '7'
-    }, {
-      name: 'New South Wales',
-      value: '1'
-    }, {
-      name: 'Northern Territory',
-      value: '8'
-    }, {
-      name: 'Queensland',
-      value: '3'
-    }, {
-      name: 'South Australia',
-      value: '5'
-    }, {
-      name: 'Tasmania',
-      value: '6'
-    }, {
-      name: 'Victoria',
-      value: '2'
-    }, {
-      name: 'Western Australia',
-      value: '4'
-    }]
+    choices: [
+      { name: 'Australia', value: '0' },
+      { name: 'Australian Capital Territory', value: '7' },
+      { name: 'New South Wales', value: '1' },
+      { name: 'Northern Territory', value: '8' },
+      { name: 'Queensland', value: '3' },
+      { name: 'South Australia', value: '5' },
+      { name: 'Tasmania', value: '6' },
+      { name: 'Victoria', value: '2' },
+      { name: 'Western Australia', value: '4' }
+    ]
   }],
   /**
      * @param {CommandInteraction} interaction
@@ -60,43 +43,17 @@ module.exports = {
       .setAuthor('Covid Information', 'https://i.imgur.com/5OJw0j7.png')
       .setTitle(`${obj[stateName].CODE} COVID Information`)
       .setThumbnail('https://i.imgur.com/5OJw0j7.png')
-      .setFields([{
-        name: 'Total Cases',
-        value: `${obj[stateName].CASE_CNT}`,
-        inline: true
-      }, {
-        name: 'Active Cases',
-        value: `${obj[stateName].ACTIVE_CNT}`,
-        inline: true
-      }, {
-        name: 'Total Deaths',
-        value: `${obj[stateName].DEATH_CNT}`,
-        inline: true
-      }, {
-        name: 'New Local Cases (Last 24h)',
-        value: `${obj[stateName].NEW_CASE_CNT}`,
-        inline: true
-      }, {
-        name: 'Tests (Last 24h)',
-        value: `${newTest}`,
-        inline: true
-      }, {
-        name: 'Deaths (Last 24h)',
-        value: `${newDeaths}`,
-        inline: true
-      }, {
-        name: 'Recovered',
-        value: `${obj[stateName].RECOV_CNT}`,
-        inline: true
-      }, {
-        name: 'New Vaccine Doses (Last 24h)',
-        value: `${newVaccine}`,
-        inline: true
-      }, {
-        name: 'Total Vaccine Doses',
-        value: `${obj[stateName].VACC_DOSE_CNT}`,
-        inline: true
-      }])
+      .setFields([
+        { name: 'Total Cases', value: `${obj[stateName].CASE_CNT}`, inline: true },
+        { name: 'Active Cases', value: `${obj[stateName].ACTIVE_CNT}`, inline: true },
+        { name: 'Total Deaths', value: `${obj[stateName].DEATH_CNT}`, inline: true },
+        { name: 'New Local Cases (Last 24h)', value: `${obj[stateName].NEW_CASE_CNT}`, inline: true },
+        { name: 'Tests (Last 24h)', value: `${newTest}`, inline: true },
+        { name: 'Deaths (Last 24h)', value: `${newDeaths}`, inline: true },
+        { name: 'Recovered', value: `${obj[stateName].RECOV_CNT}`, inline: true },
+        { name: 'New Vaccine Doses (Last 24h)', value: `${newVaccine}`, inline: true },
+        { name: 'Total Vaccine Doses', value: `${obj[stateName].VACC_DOSE_CNT}`, inline: true }
+      ])
       .setTimestamp(obj[stateName].LAST_UPDATED_DATE)
       .setFooter('Data from Covid Live (https://covidlive.com.au)')
     interaction.reply({ embeds: [stateResponse] })
