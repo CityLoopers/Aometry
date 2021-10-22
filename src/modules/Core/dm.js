0b509f496462bd7fd7c03618a0c269452ec76ac9
finneh4249 <ethancornwill@outlook.com>
2021-10-22 19:08:48 +1100

    dm users (bot owner only) command

---
+++ src/modules/Core/dm.js
@@ -0,0 +1,28 @@
+const { CommandInteraction, MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: 'dm',
+  description: 'Sends a DM to the specified user',
+  ownerOnly: true,
+  options: [{
+    name: 'user',
+    description: 'The user to send the DM to',
+    type: 'USER',
+    required: true
+  }, {
+    name: 'message',
+    description: 'The message to send',
+    type: 'STRING',
+    required: true
+  }],
+
+  async execute (interaction, client) {
+    const user = interaction.options.getUser('user')
+    const message = interaction.options.getString('message')
+    const messageEmbed = new MessageEmbed()
+      .setTitle('Message from the Bot Owner')
+      .setDescription(message)
+
+    user.send({ embeds: [messageEmbed] })
+  }
+}
