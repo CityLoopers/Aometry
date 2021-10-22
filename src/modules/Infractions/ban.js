900ec83111fc81d426eb04f6b015aa5aa45eb203
finneh4249 <ethancornwill@outlook.com>
2021-10-22 19:07:03 +1100

    moderation commands

---
+++ src/modules/Infractions/ban.js
@@ -0,0 +1,27 @@
+/* eslint-disable new-cap */
+const { CommandInteraction, MessageEmbed } = require('discord.js')
+const db = require('quick.db')
+module.exports = {
+  name: 'ban',
+  description: 'Bans the user',
+  options: [{
+    name: 'user',
+    description: 'The user to ban',
+    type: 'USER',
+    required: true
+  }, {
+    name: 'reason',
+    description: 'The reason for the ban',
+    type: 'STRING',
+    required: true
+  }],
+
+  async execute (interaction, client) {
+    const banUser = interaction.options.getUser('user')
+    const reason = interaction.options.getString('reason')
+    const infractions = new db.table(`infractions_${interaction.guild.id}`)
+    infractions.push('user', banUser)
+    infractions.push('user.reason', reason)
+    infractions.push('user.executor', interaction.user)
+  }
+}
