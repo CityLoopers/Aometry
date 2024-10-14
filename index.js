6efc7588a65dd09ce488a1572f0d4236a5c38bba
finneh4249 <ethancornwill@outlook.com>
2024-10-14 19:56:25 +1100

    create events and log in bot

---
+++ index.js
@@ -0,0 +1,28 @@
+const { BOT_TOKEN } = require('./config.js')
+
+const {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Partials
+} = require('discord.js')
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits
+const { User, Message, GuildMember, ThreadMember } = Partials
+
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [User, Message, GuildMember, ThreadMember]
+})
+
+client.config = require('./config.js')
+client.events = new Collection()
+client.commands = new Collection()
+
+const { loadEvents } = require('./handler/eventHandler')
+loadEvents(client)
+
+client
+  .login(BOT_TOKEN)
+  .catch((err) => {
+    console.log(err)
+  })
