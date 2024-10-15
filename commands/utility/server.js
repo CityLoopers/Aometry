24331c9b695bdc82a0e5044ddd594d76ea116652
Ethan Cornwill <ethancornwill@outlook.com>
2024-10-15 18:58:13 +1100

    refactor: Improve server info command in server.js

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -6,8 +6,51 @@ module.exports = {
     .setDescription('Provides information about the server.'),
   async execute (interaction) {
     // interaction.guild is the object representing the Guild in which the command was run
-    await interaction.reply(
-      `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
+    await interaction.reply({
+      embeds: [
+        {
+          color: 5814783,
+          fields: [
+            {
+              name: 'Server Owner',
+              value: `${interaction.guild.owner}`,
+              inline: true
+            },
+            {
+              name: 'Server Members',
+              value: `${interaction.guild.memberCount}`,
+              inline: true
+            },
+            {
+              name: 'Server Security',
+              value: `${interaction.guild.verificationLevel}`,
+              inline: true
+            },
+            {
+              name: 'Channels',
+              value: `${interaction.guild.channels.cache.size}`,
+              inline: true
+            },
+            {
+              name: 'Roles',
+              value: `${interaction.guild.roles.cache.size}`,
+              inline: true
+            }
+          ],
+          author: {
+            name: `${interaction.guild.name} Server Info`,
+            icon_url: `${interaction.guild.iconURL({ dynamic: true })}`
+          },
+          thumbnail: {
+            url: `${interaction.guild.iconURL({ dynamic: true })}`
+          },
+          footer: {
+            text: `Server ID: ${interaction.guild.id} | Server Created: ${interaction.guild.createdAt}`
+          }
+
+        }
+      ]
+    }
     )
   }
 }
