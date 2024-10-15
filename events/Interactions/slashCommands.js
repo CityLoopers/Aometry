b3e331c049c390fe507f35ea515b6cc954d6df08
Ethan Cornwill <ethancornwill@outlook.com>
2024-10-15 18:57:50 +1100

    chore: Improve logging in slashCommands.js

--- events/Interactions/slashCommands.js
+++ events/Interactions/slashCommands.js
@@ -10,7 +10,7 @@ module.exports = {
     if (!interaction.isChatInputCommand()) return
     const command = client.commands.get(interaction.commandName)
     if (!command) {
-      console.log(`No command found for ${interaction.commandName} interaction`)
+      console.error(`[ERR]No command found for ${interaction.commandName} interaction`)
       return interaction.reply({
         content: 'There was an error while executing this command!',
         ephemeral: true
@@ -19,14 +19,19 @@ module.exports = {
     }
 
     if (command.developer && interaction.user.id !== '125791979689869312') {
-      console.log('Developer command used')
+      console.warn(`[WARN] Developer command attempted by ${interaction.user.tag}`)
       return interaction.reply({
         content: 'You are not allowed to use this command.',
         ephemeral: true
       }
       )
     }
-    console.log(`Executing command ${interaction.commandName}`)
+
+    if (command.developer) {
+      console.warn(`[WARN] Developer command ${interaction.commandName} used`)
+    }
+
+    console.log(`[LOG] User ${interaction.user.tag} used command ${interaction.commandName}`)
     command.execute(interaction, client)
   }
 }
