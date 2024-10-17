d12f431d9a51138080c435c3d6513b83e8ef19da
Ethan Cornwill <ethancornwill@outlook.com>
2024-10-17 21:21:37 +1100

    Added check to index that checks if .env file exists, and runs the setup wizard if it doesn't

--- index.js
+++ index.js
@@ -1,56 +1,12 @@
-const {
-  Client,
-  Collection,
-  GatewayIntentBits,
-  Partials
-} = require('discord.js')
-const { Guilds, GuildMembers, GuildMessages, MessageContent } =
-  GatewayIntentBits
-const { User, Message, GuildMember, ThreadMember } = Partials
+const fs = require('fs')
 
-const client = new Client({
-  intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
-  partials: [User, Message, GuildMember, ThreadMember]
-})
+// Check if the .env file exists
+if (!fs.existsSync('.env')) {
+  // Run the interactive setup
+  require('./setup.js')
+} else {
+  // Start the bot client
+  require('./client.js')
+}
 
-client.botVersion = require('./package.json').version
-client.config = require('./config.js')
-client.events = new Collection()
-client.commands = new Collection()
-client.subCommands = new Collection()
-
-// TODO: Add check for update of installed repositories and / or modules
-
-require('./handler/llmHander.js')
-
-const { loadEvents } = require('./handler/eventHandler')
-loadEvents(client)
-
-const { connect } = require('mongoose')
-connect(client.config.DB_URL, {})
-  .then(() => console.log('✅ Mongoose connected'))
-  .catch((err) => console.log(err))
-client
-  .login(client.config.BOT_TOKEN)
-  .then(() => {
-    console.log('✅ Error Handler loaded')
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-
-process.on('unhandledRejection', (err) => {
-  client.channels.cache
-    .get(client.config.SYSTEM_LOGS_CHANNEL)
-    .send({
-      embeds: [
-        {
-          title: 'Unhandled Rejection',
-          description: err.toString(),
-          color: 0xff0000,
-          timestamp: new Date()
-        }
-      ]
-    })
-  console.error(err)
-})
+require('./Utilities/cliManager.js')
