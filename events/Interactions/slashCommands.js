d688abd39ef054b22d0663f6a583d3e4788ee0af
finneh4249 <ethancornwill@outlook.com>
2024-10-17 15:43:58 +1100

    refactor: Add subCommands support to commandHandler module
    This commit adds the ability to handle subCommands in the commandHandler module. It includes changes to the loadCommands function to support subCommands. Additionally, it adds a check for missing data property in commands and logs a warning if it's missing.

--- events/Interactions/slashCommands.js
+++ events/Interactions/slashCommands.js
@@ -10,28 +10,49 @@ module.exports = {
     if (!interaction.isChatInputCommand()) return
     const command = client.commands.get(interaction.commandName)
     if (!command) {
-      console.error(`[ERR]No command found for ${interaction.commandName} interaction`)
+      console.error(
+        `[ERR] No command found for ${interaction.commandName} interaction`
+      )
       return interaction.reply({
         content: 'There was an error while executing this command!',
         ephemeral: true
-      }
-      )
+      })
     }
 
-    if (command.developer && interaction.user.id !== '125791979689869312') {
-      console.warn(`[WARN] Developer command attempted by ${interaction.user.tag}`)
+    if (command.developer && interaction.user.id !== client.config.DEV_ID) {
+      console.warn(
+        `[WARN] Developer command attempted by ${interaction.user.tag}`
+      )
       return interaction.reply({
         content: 'You are not allowed to use this command.',
         ephemeral: true
-      }
+      })
+    }
+
+    const subCommand = interaction.options.getSubcommand()
+    if (subCommand) {
+      const subCommandFile = client.subCommands.get(
+        `${interaction.commandName}.${subCommand}`
       )
+      if (!subCommandFile) {
+        console.error(
+          `[ERR] No sub command found for ${interaction.commandName}.${subCommand} interaction`
+        )
+        return interaction.reply({
+          content: 'There was an error while executing this command!',
+          ephemeral: true
+        })
+      }
+      subCommandFile.execute(interaction, client)
     }
 
     if (command.developer) {
       console.warn(`[WARN] Developer command ${interaction.commandName} used`)
     }
 
-    console.log(`[LOG] User ${interaction.user.tag} used command ${interaction.commandName}`)
+    console.log(
+      `[LOG] User ${interaction.user.tag} used command ${interaction.commandName}`
+    )
     command.execute(interaction, client)
   }
 }
