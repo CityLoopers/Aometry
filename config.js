16e6b0a91b0c79da24aed625d0de00f9539b9eb4
finneh4249 <ethancornwill@outlook.com>
2024-10-17 15:43:14 +1100

    refactor: Update config.js to include additional required environment variables
    This commit updates the config.js file to include additional required environment variables: DB_URL and DEV_ID. These variables were missing from the previous implementation, causing errors when the application was run. By adding these variables to the config.js file, the application can now access the necessary database URL and developer ID, ensuring proper functionality.

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
 require('dotenv').config()
 
-if (!process.env.BOT_TOKEN) {
-  throw new Error('Missing BOT_TOKEN in .env')
-}
-if (!process.env.LOGS_CHANNEL) {
-  throw new Error('Missing LOGS_CHANNEL in .env')
+const requiredConfigs = [
+  'BOT_TOKEN',
+  'LOGS_CHANNEL',
+  'DB_URL',
+  'DEV_ID'
+]
+for (const config of requiredConfigs) {
+  if (!process.env[config]) {
+    throw new Error(`Missing ${config} in .env`)
+  }
 }
 
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
-  LOGS_CHANNEL: process.env.LOGS_CHANNEL
+  LOGS_CHANNEL: process.env.LOGS_CHANNEL,
+  DB_URL: process.env.DB_URL,
+  DEV_ID: process.env.DEV_ID
 }
