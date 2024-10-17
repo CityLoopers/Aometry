b55ef6fa5809b6d76b4ac688c80d4fdecbdcb488
finneh4249 <ethancornwill@outlook.com>
2024-10-17 17:44:43 +1100

    refactor: Move repositoryDownloader.js to Utilities folder
    This commit moves the repositoryDownloader.js file to the Utilities folder. This change improves the organization of the codebase by grouping related files together.

---
+++ Utilities/repositoryDownloader.js
@@ -0,0 +1,5 @@
+// Steps:
+// 1. Clone the repository
+// 2. Parse the repository using the repositories info.json file
+// 3. Move the files to the correct location inside the bots structure
+// 4. Delete the repository
